5bcd4ec606dde9d2fd54fbd46f1ae0d76a42563f
Emmalcd <joselemmanuel9@gmail.com>
2024-01-20 15:29:27 -0600

    Ejercicio hola mundo finalizado

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ejercicio1
 {
@@ -10,6 +6,9 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo!!!");
+            Console.WriteLine("Hola Emma");
+            Console.ReadLine();
         }
     }
 }
